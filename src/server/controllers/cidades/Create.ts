084829d1dc2770c779480c9f81386582c30276a7
underline84 <evanfatu@hotmail.com>
2022-10-13 20:14:30 -0300

    13-10-2022

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,11 +1,34 @@
 import { Request, Response } from "express";
+import * as yup from 'yup';
+import { StatusCodes } from 'http-status-codes';
+import '../../shared/services/TranslationsYup';
 
 interface ICidade {
     nome: string;
+    estado: string;
 }
 
-export const create = (req: Request<{},{}, ICidade>, res: Response) => {
-    //const data: ICidade = req.body;   
+const bodyValidation: yup.SchemaOf<ICidade> = yup.object().shape({
+    nome: yup.string().required().min(3),
+    estado: yup.string().required().min(3),
+});
+
+export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
+    let validatedData: ICidade | undefined = undefined;
+
+    try {
+        validatedData = await bodyValidation.validate(req.body);
+    } catch (err) {
+        const yupError = err as yup.ValidationError;
+        const validationErrors: Record<string, string> = {};
+
+        yupError.inner.forEach(error => {
+            if (error.path === undefined) return;
+            validationErrors[error.path] = error.message;
+        });
+
+        return res.status(StatusCodes.BAD_REQUEST).json({ err });
+    }
 
     return res.send(req.body.nome);
 }
